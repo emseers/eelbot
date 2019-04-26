5ef72c3f4cafc96cda59daf282a0e0817f86d817
boarnoah <adipamihi@gmail.com>
2019-04-25 20:15:19 -0400

    Add config and config parsing

--- eelbot.py
+++ eelbot.py
@@ -2,5 +2,28 @@
     EelBot
 '''
 
+import argparse
+import configparser
+
+CONFIG = {}
+
+
 def poll():
     pass
+
+
+parser = argparse.ArgumentParser(description="Discord bot, listener server")
+parser.add_argument("-c", "--config", default="config.ini")
+
+config = configparser.ConfigParser()
+
+if __name__ == '__main__':
+    print("Starting EelBot Discord bot")
+
+    args = parser.parse_args()
+
+    config.read(args.config)
+
+    CONFIG["POLL_TIME"] = config["GENERAL"].get("POLL_TIME", 1)
+
+    poll()
